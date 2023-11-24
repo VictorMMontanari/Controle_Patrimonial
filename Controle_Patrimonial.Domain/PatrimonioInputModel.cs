7e43160f4b153de3614497208b4d373ae72bd3dc
VictorMmontanari <victormm9000@gmail.com>
2023-11-24 19:11:33 -0300

    Ajustes do projeto

--- Controle_Patrimonial.Domain/PatrimonioInputModel.cs
+++ Controle_Patrimonial.Domain/PatrimonioInputModel.cs
@@ -16,4 +16,18 @@ namespace Controle_Patrimonial.Domain
         public int UsuarioId { get; set; }
         public int BlocoId { get; set; }
     }
+
+    public class PatrimonioPutModel
+    {
+        public int PatrimonioId { get; set; }
+        public string CodigoBarra { get; set; }
+        public string Tipo { get; set; }
+        public string Nome { get; set; }
+        public float Valor { get; set; }
+        public string NotaFiscal { get; set; }
+        public bool Ativo { get; set; }
+        public DateTime DataRegistro { get; set; }
+        public int UsuarioId { get; set; }
+        public int BlocoId { get; set; }
+    }
 }
--- Controle_Patrimonial_API/Program.cs
+++ Controle_Patrimonial_API/Program.cs
@@ -6,7 +6,6 @@ using Infra.SqlServer.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
-
 // Injection of Controll
 builder.Services.AddScoped<IUsuarioRepository, UsuarioRepository>();
 builder.Services.AddScoped<IPatrimonioRepository, PatrimonioRepository>();
@@ -20,20 +19,16 @@ builder.Services.AddScoped<ApplicationServiceEnviarCheckIn, ApplicationServiceEn
 builder.Services.AddScoped<Login, Login>();
 builder.Services.AddScoped<LoginService, LoginService>();
 builder.Services.AddScoped<SqlServerContex, SqlServerContex>();
+
 // Add services to the container.
-
-
-//Importante para rodar
-builder.Services.AddCors();
-
 builder.Services.AddCors(p => p.AddPolicy("corsapp", builder =>
 {
     builder.WithOrigins("*").AllowAnyMethod().AllowAnyHeader();
 }));
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -55,10 +50,3 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
-
-app.UseCors(c =>
-{
-    c.AllowAnyHeader();
-    c.AllowAnyMethod();
-    c.AllowAnyOrigin();
-});
